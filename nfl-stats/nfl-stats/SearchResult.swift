d5e5930352bcec8a3b1c794063813a00c7729f79
Pedro Henrique Pereira Lima <phenriquelima.ph@gmail.com>
2017-12-13 21:01:57 -0200

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ nfl-stats/nfl-stats/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  nfl-stats
+//
+//  Created by Pedro Henrique Pereira Lima on 13/12/17.
+//  Copyright © 2017 Pedro Henrique Pereira Lima. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var firstName = ""
+    var lastName = ""
+}
